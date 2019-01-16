8a1b123ebf7a6c358bf030212e97052ad5094dc9
hackeryarn <artemchernyak@gmail.com>
2019-01-16 06:02:31 -0600

    padding the date

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,30 +1,27 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
 
 module Lib
-    ( newFile
-    ) where
+  ( newFile
+  ) where
 
-import Data.Time.Clock
-import Data.Time.Calendar
-import Data.Text (Text)
-import qualified Data.Text.IO as IO
-import qualified Data.Text as T
-import Data.Char (isNumber)
-
+import           Data.Char          (isNumber)
+import           Data.Text          (Text)
+import qualified Data.Text          as T
+import qualified Data.Text.IO       as IO
+import           Data.Time.Calendar
+import           Data.Time.Clock
 
 newFile :: String -> IO ()
 newFile file = do
   content <- IO.readFile file
-  currentDate <- getCurrentTime >>= return . toGregorian . utctDay
+  currentDate <- toGregorian . utctDay <$> getCurrentTime
   let ls = T.lines content
       day = show $ getDay file + 1
       newFileName = "day" ++ day ++ ".md"
-      newContent = map (setDay day). map (setDate currentDate) $ deleteBody ls 
-   in do
-      IO.writeFile newFileName $ T.unlines newContent
-
+      newContent = map (setDay day . setDate currentDate) $ deleteBody ls
+   in IO.writeFile newFileName $ T.unlines newContent
 
 setDate :: (Integer, Int, Int) -> Text -> Text
 setDate date line
@@ -33,17 +30,20 @@ setDate date line
 
 getDateText :: (Integer, Int, Int) -> Text
 getDateText (year, month, day) =
-  T.intercalate "-" $ map (T.pack . show) [year, toInteger month, toInteger day]
+  T.intercalate "-" $
+  map (T.pack . padTwo . show) [year, toInteger month, toInteger day]
+
+padTwo :: String -> String
+padTwo n = replicate (2 - length n) '0' ++ n
 
 setDay :: String -> Text -> Text
 setDay day line
-  | T.isPrefixOf "day: " line = T.append "day: " $ T.pack day 
+  | T.isPrefixOf "day: " line = T.append "day: " $ T.pack day
   | otherwise = line
 
-
 deleteBody :: [Text] -> [Text]
-deleteBody [] = []
-deleteBody (h:t) = h : (takeWhile (/= "---") t) ++ ["---"]
+deleteBody []    = []
+deleteBody (h:t) = h : takeWhile (/= "---") t ++ ["---"]
 
 getDay :: String -> Int
 getDay = read . filter isNumber
